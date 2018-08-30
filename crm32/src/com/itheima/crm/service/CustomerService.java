19d0387cd23825ec41e3e2413f77ca5b6aad8cd2
xiayang2018 <251303336@qq.com>
2018-08-30 17:54:05 +0800

    cc

---
+++ crm32/src/com/itheima/crm/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.itheima.crm.service;
+
+import com.itheima.common.utils.Page;
+import com.itheima.crm.pojo.Customer;
+import com.itheima.crm.pojo.QueryVo;
+
+public interface CustomerService {
+	
+	//通过四个条件  查询分页对象 
+		public Page<Customer> selectPageByQueryVo(QueryVo vo);
+		
+		//通过ID查询客户
+		public Customer selectCustomerById(Integer id);
+		
+		//修改客户通过ID
+		public void updateCustomerById(Customer customer);
+}
